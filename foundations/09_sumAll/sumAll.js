16ae3c0f6c1c77016f73fa092165e269851671c1
marin-hyatt <marin.hyatt@gmail.com>
2026-06-13 21:22:04 -0400

    feat: sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (a >= b) {
+    return sumTwo(a, b);
+  } else {
+    return sumTwo(b, a);
+  }
+};
 
+const sumTwo = (bigger, smaller) => {
+  let arr = [];
+
+  for (let i = smaller; i <= bigger; i++) {
+    arr.push(i);
+  }
+
+  let res = 0;
+  const initialValue = 0;
+  const sumWithInitial = arr.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    initialValue,
+  );
+
+  return sumWithInitial;
 };
 
 // Do not edit below this line
