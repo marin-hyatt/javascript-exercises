8bcd319025476d8e45848596f3b8196587c2002d
marin-hyatt <marin.hyatt@gmail.com>
2026-06-13 21:02:47 -0400

    feat: removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...rest) {
+  rest.forEach((element) => {
+    const index = arr.indexOf(element);
+
+    if (index !== -1) {
+      arr.splice(index, 1);
+    }
+  });
+
+  return arr;
 };
 
 // Do not edit below this line
